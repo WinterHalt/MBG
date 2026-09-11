4c4e131bf4b061771a99876d21e4db9085233cde
Kelnit <kelvinmulyawan2000@gmail.com>
2026-09-11 14:10:43 +0700

    Hi

--- application/libraries/PHPJasperXML_lib.php
+++ application/libraries/PHPJasperXML_lib.php
@@ -1,9 +1,7 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 
-// Ensure the path to the original vendor library is correct
-include_once APPPATH . 'libraries/phpjasperxml/PHPJasperXML.inc.php';
+include_once APPPATH . '/libraries/phpjasperxml/PHPJasperXML.inc.php';
 
-// The class name must start with a capital letter and match the filename
-class Phpjasperxml_lib extends PHPJasperXML {
-    
+class PHPJasperXML_lib extends PHPJasperXML {
+	
 }
